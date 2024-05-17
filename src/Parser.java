b76e58e798ad750ffc40ce8208b39fa42f6f421a
Pablo Uscanga <0225717@up.edu.mx>
2024-05-17 17:58:27 -0600

    Added tree logic

--- src/Parser.java
+++ src/Parser.java
@@ -17,11 +17,11 @@ public class Parser {
     }
 
     //print())
-    
-    
-    private static void error1(int type) {
+
+
+    private static void error(int type) {
         if (isCurrentTokenValid()) {
-            String msg = "\nError msg: Line: " + (tokens.get(currentToken).getLine() - 1) + ": expected";
+            String msg = "\nError msg: Line: " + (tokens.get(currentToken).getLine() - 1) + " expected ";
             switch (type) {
                 case 1:
                     msg += "{";
@@ -68,7 +68,7 @@ public class Parser {
         }
     }
 
-    
+
     //run
     public DefaultMutableTreeNode parse(){
         currentToken = 0;
@@ -76,7 +76,7 @@ public class Parser {
         current_level = root;
         RULE_PROGRAM();
 
-        return root;   
+        return root;
     }
 
     private static void clearTree(DefaultMutableTreeNode node){
@@ -88,7 +88,16 @@ public class Parser {
         current_level.add(newNode);
         current_level = newNode;
     }
-    
+
+    private static void addNote(String name, boolean changeLevel){
+        DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(name);
+        current_level.add(newNode);
+        if(changeLevel){
+            current_level = newNode;
+        }
+
+    }
+
 
     private static boolean isCurrentTokenValid() {
         return currentToken >= 0 && currentToken < tokens.size();
@@ -112,46 +121,55 @@ public class Parser {
 
     public static void RULE_PROGRAM() {
         //NODE
-        addNote("pabuaghmandou");
+        addNote("PROGRAM");
 
         if (tokens.get(currentToken).getWord().equals("{") && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         } else {
-            error();
+            error(1);
         }
 
         RULE_BODY();
 
         if (tokens.get(currentToken).getWord().equals("}")) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         } else {
-            error();
+            error(2);
         }
     }
 
-    
+
 
     public static void RULE_BODY() {
         //Añade body al árbol
+        addNote("BODY");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
 
         while (!tokens.get(currentToken).getWord().equals("}") && isCurrentTokenValid()) {
             if (tokens.get(currentToken).getToken().equals("ID") && isCurrentTokenValid()) {
                 RULE_ASSIGNMENT();
                 if (tokens.get(currentToken).getWord().equals(";") && isCurrentTokenValid()) {
                     //Añadir ; al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                 } else {
                     System.out.println("Error 3"); // Add error to console
+                    error(3);
                 }
             } else if (isVariable(tokens.get(currentToken).getWord()) && tokens.get(currentToken).getToken().equals("KEYWORD") && isCurrentTokenValid()) {
                 RULE_VARIABLE();
                 if (tokens.get(currentToken).getWord().equals(";") && isCurrentTokenValid()) {
                     //Añadir ; al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                 } else {
                     System.out.println("Error 3"); // Add error to console
+                    error(3);
                 }
             } else if (tokens.get(currentToken).getWord().equals("while") && isCurrentTokenValid()) {
                 RULE_WHILE();
@@ -161,263 +179,365 @@ public class Parser {
                 RULE_RETURN();
                 if (tokens.get(currentToken).getWord().equals(";") && isCurrentTokenValid()) {
                     //Añadir ; al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                 } else {
                     System.out.println("Error 3"); // Add error to console
+                    error(3);
                 }
             } else if (tokens.get(currentToken).getWord().equals("print") && isCurrentTokenValid()) {
                 RULE_PRINT();
                 if (tokens.get(currentToken).getWord().equals(";") && isCurrentTokenValid()) {
                     //Añadir ; al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                 } else {
                     System.out.println("Error 3"); // Add error to console
+                    error(3);
                 }
             }else {
                 System.out.println("Error 4"); // Add error to console
+                error(6);
             }
         }
         //devuelve el nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_ASSIGNMENT() {
         //Añade assignment al árbol
+        addNote("RULE_ASSIGNMENT");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
 
         if (tokens.get(currentToken).getToken().equals("ID") && isCurrentTokenValid()) {
             //Añadir id(la palabra) al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             if (tokens.get(currentToken).getWord().equals("=") && isCurrentTokenValid()) {
                 //Añade = al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
                 RULE_EXPRESSION();
             } else {
                 System.out.println("Error 7"); // Add error to console
+                error(7);
             }
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_EXPRESSION() {
         //Añade expression al árbol
+        addNote("RULE_EXPRESSION");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
 
         RULE_X();
 
         while (tokens.get(currentToken).getWord().equals("|") && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_X();
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_X() {
         //Añade X al árbol
+        addNote("RULE_X");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         RULE_Y();
 
         while (tokens.get(currentToken).getWord().equals("&") && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_Y();
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_Y() {
         //A{ade Y al árbol
+        addNote("RULE_Y");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         if (tokens.get(currentToken).getWord().equals("!") && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         }
 
         RULE_R();
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
+
     }
 
     public static void RULE_R() {
         //Añade R al árbol
+        addNote("RULE_R");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         RULE_E();
 
         while (tokens.get(currentToken).getWord().equals("<") | tokens.get(currentToken).getWord().equals(">")
                 | tokens.get(currentToken).getWord().equals("==") | tokens.get(currentToken).getWord().equals("!=")
                 | tokens.get(currentToken).getWord().equals("<=") | tokens.get(currentToken).getWord().equals(">=")) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_E();
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_E() {
         //Añade E al árbol
+        addNote("RULE_E");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
+
         RULE_A();
 
         while ((tokens.get(currentToken).getWord().equals("-") | tokens.get(currentToken).getWord().equals("+")) && isCurrentTokenValid()) {
             //Añadir current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_A();
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_A() {
         //Añade A al árbol
+        addNote("RULE_A");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         RULE_B();
 
         while ((tokens.get(currentToken).getWord().equals("/") | tokens.get(currentToken).getWord().equals("*")) && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_B();
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_B() {
         //A{ade B al árbol
+        addNote("RULE_B");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
 
         if (tokens.get(currentToken).getWord().equals("-") && isCurrentTokenValid()) {
             //Añade - al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         }
 
         RULE_C();
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_C() {
         //Añade C al árbol
+        addNote("RULE_C");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
 
         if (isVarKey(tokens.get(currentToken).getToken()) && isCurrentTokenValid()) {
             //Añadir current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         } else if (tokens.get(currentToken).getToken().equals("ID") && isCurrentTokenValid()) {
             //Añadir current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         } else if (tokens.get(currentToken).getToken().equals("KEYWORD") && isCurrentTokenValid()) {
             //Añadir current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
         } else if (tokens.get(currentToken).getWord().equals("(") && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_EXPRESSION();
             if (tokens.get(currentToken).getWord().equals(")") && isCurrentTokenValid()) {
                 //Añade current token al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
             } else {
-                error();
+                error(4);
             }
         } else {
-            error();
+            error(5);
             currentToken++;
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_VARIABLE() {
         //Añade Variable al árbol
+        addNote("RULE_VARIABLE");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         if ((isVariable(tokens.get(currentToken).getWord()) && tokens.get(currentToken).getToken().equals("KEYWORD")) && isCurrentTokenValid()) {
             //Añade current token al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             if (tokens.get(currentToken).getToken().equals("ID") && isCurrentTokenValid()) {
                 //Añadir current token al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
             } else {
-                error();
+                error(8);
             }
             if (tokens.get(currentToken).getWord().equals("=") && isCurrentTokenValid()) {
                 //Añade current token al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
                 RULE_EXPRESSION();
             }
         }
         //Devuelve nivel al árbol
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_WHILE() {
         //Añade While al árbol
+        addNote("RULE_WHILE");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
 
         if (tokens.get(currentToken).getWord().equals("while") && isCurrentTokenValid()) {
             //Añade while al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             if (tokens.get(currentToken).getWord().equals("(") && isCurrentTokenValid()) {
                 //Añade ( al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
                 RULE_EXPRESSION();
                 if (tokens.get(currentToken).getWord().equals(")") && isCurrentTokenValid()) {
                     //Añade ) al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                     RULE_PROGRAM();
                 }
                 else {
-                    error();
+                    error(4);
                 }
             } else {
-                error();
+                error(9);
             }
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_IF() {
         //Añade IF al árbol
+        addNote("RULE_IF");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         if (tokens.get(currentToken).getWord().equals("if") && isCurrentTokenValid()) {
             //Añade if al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             if (tokens.get(currentToken).getWord().equals("(") && isCurrentTokenValid()) {
                 //Añade ( al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
                 RULE_EXPRESSION();
                 if (tokens.get(currentToken).getWord().equals(")")) {
                     //Añade ) al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                     RULE_PROGRAM();
                     if (tokens.get(currentToken).getWord().equals("else")) {
                         //Añade else al árbol
+                        addNote(tokens.get(currentToken).getWord(), false);
                         currentToken++;
                         RULE_PROGRAM();
                     }
                 } else {
-                    error();
+                    error(4);
                 }
             } else {
-                error();
+                error(9);
             }
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_RETURN() {
         //Añade Return al árbol
+        addNote("RULE_RETURN");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         if (tokens.get(currentToken).getWord().equals("return") && isCurrentTokenValid()) {
             //Añade return al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
             RULE_EXPRESSION();
         }
         //Devuelve nivel al padre
+        current_level = current_level_at_tree;
     }
 
     public static void RULE_PRINT() {
         //Añade Print al árbol
+        addNote("RULE_PRINT");
+
+        DefaultMutableTreeNode current_level_at_tree = (DefaultMutableTreeNode) current_level.getParent();
+
         if (tokens.get(currentToken).getWord().equals("print") && isCurrentTokenValid()) {
             //Añade print al árbol
+            addNote(tokens.get(currentToken).getWord(), false);
             currentToken++;
 
             if (tokens.get(currentToken).getWord().equals("(") && isCurrentTokenValid()) {
                 //Añade ( al árbol
+                addNote(tokens.get(currentToken).getWord(), false);
                 currentToken++;
                 RULE_EXPRESSION();
                 if (tokens.get(currentToken).getWord().equals(")") && isCurrentTokenValid()) {
                     //Añade ) al árbol
+                    addNote(tokens.get(currentToken).getWord(), false);
                     currentToken++;
                 } else {
-                    error1(4);
+                    error(4);
                 }
             } else {
-                error1(9);
+                error(9);
             }
         }
+        current_level = current_level_at_tree;
     }
 
-    public static void error() {
-        System.out.println("Error in " + tokens.get(currentToken).getLine());
-    }
+    // public static void error() {
+    //     System.out.println("Error in " + tokens.get(currentToken).getLine());
+    // }
 }
